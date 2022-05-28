cb2ef8a8f3e71914cbffbae01a7805629a61fec5
Trương Huỳnh Quý <80085196+truonghuynhquy@users.noreply.github.com>
2022-05-28 08:40:23 +0700

    19522110

--- src/index.js
+++ src/index.js
@@ -9,14 +9,20 @@ const port = 3000;
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //console.log('PATH', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {    
   res.render('home');
+})
+
+app.get('/news', (req, res) => {    
+  res.render('news');
 })
 
 app.listen(port, () => {
